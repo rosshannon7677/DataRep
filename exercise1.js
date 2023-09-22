f90ef53f4625e53406b282eae0f3b802089fb90d
Ross Hannon <rosshannonty@gmail.com>
2023-09-22 11:09:10 +0000

    first commit (functions)

---
+++ exercise1.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+
+// functions
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function SayHello(){
+    console.log("Hi");
+}
+
+myFunction();
+
+const myNewFunction = function(){
+    console.log("My new function");
+}
+
+myNewFunction();
+
+const myComponent = ()=>{
+    console.log("Bye");
+}
+
+myComponent();
